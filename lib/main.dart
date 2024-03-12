5d19d95a1b70b80160c8ce69fd1a84dab007404a
Frostonyx <danielhelm18@gmail.com>
2024-03-12 14:56:05 -0700

    added mana images

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,7 @@ class MagicTrackerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Magic Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.dark(),
       home: MagicTrackerScreen(),
     );
   }
@@ -27,6 +25,16 @@ class MagicTrackerScreen extends StatefulWidget {
 class _MagicTrackerScreenState extends State<MagicTrackerScreen> {
   int life = 20;
   int stormCount = 0;
+
+  Map<String, bool> manaVisibility = {
+    'Red': true,
+    'Blue': true,
+    'Black': true,
+    'Green': true,
+    'White': true,
+    'Colorless': true,
+  };
+
   Map<String, int> manaCount = {
     'Red': 0,
     'Blue': 0,
@@ -75,38 +83,66 @@ class _MagicTrackerScreenState extends State<MagicTrackerScreen> {
           : manaCount[color] = (manaCount[color] ?? 0) - 1;
     });
   }
+  //DEPRECIATED:
 
-  Color _getColorFromManaType(String manaType) {
-    switch (manaType) {
-      case 'Red':
-        return Colors.red;
-      case 'Blue':
-        return Colors.blue;
-      case 'Black':
-        return Colors.black;
-      case 'Green':
-        return const Color.fromARGB(255, 18, 54, 20);
-      case 'White':
-        return Colors.white;
-      case 'Colorless':
-        return Colors.grey;
-      default:
-        return Colors.black; // Default color
-    }
-  }
+  //Color _getColorFromManaType(String manaType) {
+  //  switch (manaType) {
+  //    case 'Red':
+  //      return Colors.red;
+  //    case 'Blue':
+  //      return Colors.blue;
+  //    case 'Black':
+  //      return Colors.black;
+  //    case 'Green':
+  //      return Color.fromARGB(255, 55, 179, 61);
+  //    case 'White':
+  //      return Colors.white;
+  //    case 'Colorless':
+  //      return const Color.fromARGB(255, 204, 204, 204);
+  //    default:
+  //      return Colors.black; // Default color
+  //  }
+  //}
 
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double halfScreenHeight = screenHeight / 2;
-    double quarterScreenHeight = screenHeight / 6;
+    double quarterScreenHeight = screenHeight / 7;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Magic Tracker'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('MtG Tracker'),
+      ),
+       drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Mana Counters',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            for (var manaType in manaVisibility.keys)
+              CheckboxListTile(
+                title: Text(manaType),
+                value: manaVisibility[manaType],
+                onChanged: (value) {
+                  setState(() {
+                    manaVisibility[manaType] = value!;
+                  });
+                },
+              ),
+          ],
+        ),
       ),
       body: Container(
-        color: const Color.fromARGB(255, 56, 56, 56),
+        color: const Color.fromARGB(255, 138, 137, 137),
         child: Column(
           children: [
             GestureDetector(
@@ -121,64 +157,71 @@ class _MagicTrackerScreenState extends State<MagicTrackerScreen> {
               onLongPress: _resetLife,
               child: Container(
                 height: halfScreenHeight,
-                color: const Color.fromARGB(255, 56, 56, 56),
+                color: Color.fromARGB(255, 138, 137, 137),
                 child: Center(
                   child: Text(
                     '$life',
-                    style: TextStyle(fontSize: 164),
+                    style: const TextStyle(fontSize: 300, fontFamily: 'HUGEBRUSH'),
                   ),
                 ),
               ),
             ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: const Color.fromARGB(255, 56, 56, 56),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: manaCount.keys.map((color) {
-                        return Column(
-                          children: [
-                            GestureDetector(
-                              onTap: () => _updateMana(
-                                  color, false), // Increase when tapped
-                              onLongPress: _resetStormAndMana,
-                              child: Text(
-                                '${manaCount[color] ?? 0}',
-                                style: TextStyle(fontSize: 64),
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () => _updateMana(
-                                  color, true), // Decrease when tapped
-                              onLongPress: _resetStormAndMana,
-                              child: Icon(Icons.circle,
-                                  color: _getColorFromManaType(color), size: 64),
-                            ),
-                          ],
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                                  Container(
-                    height: quarterScreenHeight,
-                    color: const Color.fromARGB(255, 56, 56, 56),
-                    child: GestureDetector(
-                      onTap: _updateStormCount,
-                      onLongPress: _resetStormAndMana,
-                      child: Center(
+          
+                Expanded(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: [
+      Expanded(
+        child: Center(
+          child: Container(
+            color: const Color.fromARGB(255, 138, 137, 137),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: manaCount.keys.where((color) => manaVisibility[color]!).map((color) {
+
+                  return Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () => _updateMana(color, false),
+                        onLongPress: _resetStormAndMana,
                         child: Text(
-                          'Storm Count: $stormCount',
-                          style: TextStyle(fontSize: 24),
+                          '${manaCount[color] ?? 0}',
+                          style: TextStyle(fontSize: 64),
                         ),
                       ),
-                    ),
-                  ),
-                ],
-              ),
+                      GestureDetector(
+                        onTap: () => _updateMana(color, true),
+                        onLongPress: _resetStormAndMana,
+                        child: Image.asset('assets/images/$color.png',
+                             height: 64, width: 64),
+                      ),
+                    ],
+                  );
+              }).toList(),
             ),
+          ),
+        ),
+      ),
+      Container(
+        height: quarterScreenHeight,
+        color: const Color.fromARGB(255, 138, 137, 137),
+        child: GestureDetector(
+          onTap: _updateStormCount,
+          onLongPress: _resetStormAndMana,
+          child: Center(
+            child: Text(
+              'Storm Count: $stormCount',
+              style: TextStyle(fontSize: 36),
+            ),
+          ),
+        ),
+      ),
+    ],
+  ),
+),
+
           ],
         ),
       ),
